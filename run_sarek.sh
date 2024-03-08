598d8f621dd6df381a08fd478bfa7911d1237fa7
Clemens Hug <clemens.hug@gmail.com>
2024-03-08 10:40:36 -0500

    Add job ID suffix to report and timeline files

--- run_sarek.sh
+++ run_sarek.sh
@@ -14,5 +14,6 @@ set -x
 
 nextflow run nf-core/sarek -r 3.4.0 -profile singularity \
   -params-file nf-params.json -c ~/o2.config \
-  -with-trace -with-report report.html -with-timeline timeline.html \
+  -with-trace -with-report "report_${SLURM_JOB_ID}.html" \
+  -with-timeline "timeline_${SLURM_JOB_ID}.html" \
   "$@"
